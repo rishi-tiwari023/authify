0005231bbfd163d5d481ecd139b8cb82ef8c2b53
Rishi Tiwari <umesh896073@gmail.com>
2025-10-31 23:49:41 +0530

    backend: add UserRepository for user persistence
    Wraps TypeORM repository for User entity

---
+++ backend/src/repository/UserRepository.ts
@@ -0,0 +1,25 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../config/data-source';
+import { User } from '../model/User';
+
+export class UserRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.repository.findOne({ where: { email } });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await this.repository.findOne({ where: { id } });
+  }
+
+  async create(userData: Partial<User>): Promise<User> {
+    const user = this.repository.create(userData);
+    return await this.repository.save(user);
+  }
+}
+
