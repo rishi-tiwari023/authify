ce15434713a532cde878b2d26e441f9b9e642a06
Rishi Tiwari <umesh896073@gmail.com>
2025-11-18 23:35:40 +0530

    Add Jest configuration for TypeScript testing

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  coverageDirectory: 'coverage',
+  verbose: true,
+};
+
